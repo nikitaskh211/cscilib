76bf30a93d279f50971f1369e1aa6d18a88de0c9
Никита Схабицкий <nikitaskh211@gmail.com>
2026-07-30 22:05:22 +0300

    Added datatypes table.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cscilib
 This is a pure C scientific library.
+
+## What is it ?
+This is a collection of structures and functions for scientific computations. 
+
+## What is included ?
+Array structures, discretization and numerical methods, you name it.
+
+## Why it exists ?
+Mostly to replace existing GSL as it is outdated and slow.
---
+++ core/include/sl-types.h
@@ -0,0 +1,61 @@
+#ifndef SL_TYPES_H
+#define SL_TYPES_H
+
+/* ---- Necessary includes ---- */
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <complex.h>
+
+/* ---- X-macro expansion safety ---- */
+#ifdef X
+#undef X
+#endif // !X
+
+/* ---- Single source of truth ---- */
+#define SL_TYPES                                        \
+    X(B8, bool, b8, "boolean")                          \
+    X(U8, uint8_t, u8, "unsigned_8bit_int")             \
+    X(U16, uint16_t, u16, "unsigned_16bit_int")         \
+    X(U32, uint32_t, u32, "unsigned_32bit_int")         \
+    X(UOS, size_t, uos, "unsigned_osbit_int")           \
+    X(U64, uint64_t, u64, "unsigned_64bit_int")         \
+    X(I8, int8_t, i8, "8bit_int")                       \
+    X(I16, int16_t, i16, "16bit_int")                   \
+    X(I32, int32_t, i32, "32bit_int")                   \
+    X(I64, int64_t, i64, "64bit_int")                   \
+    X(F32, float, f32, "32bit_float")                   \
+    X(F64, double, f64, "64bit_float")                  \
+    X(CF32, float complex, cf32, "32bit_complex_float") \
+    X(CF64, double complex, cf64, "64bti_complex_float")
+
+/* ---- Type aliases ---- */
+#define X(tag, ctype, alias, name) typedef ctype alias;
+SL_TYPES
+#undef X
+
+/* ---- Enumeration for types ---- */
+#define X(tag, ctype, alias, name) TYPE_##tag,
+typedef enum
+{
+    SL_TYPES
+        SL_TYPES_COUNT
+} type_tag;
+#undef X
+
+/* ---- Lookup table for type sizes in BYTES ---- */
+#define X(tag, ctype, alias, name) [TYPE_##tag] = sizeof(ctype),
+static const uos type_sizes[] = {SL_TYPES};
+#undef X
+
+/* ---- Lookup table for type names ---- */
+#define X(tag, ctype, alias, name) [TYPE_##tag] = name,
+static const char *type_names[] = {SL_TYPES};
+#undef X
+
+/* ---- X-macro expansion safety ---- */
+#ifdef X
+#undef X
+#endif // !X
+
+#endif // !SL_TYPES_H
